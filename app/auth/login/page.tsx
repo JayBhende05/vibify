2576e0328d7a7fe27e4e8cfff5ff6a90a62a09b3
JayBhende05 <bhendejay5.bms@gmail.com>
2026-04-22 08:17:24 +0530

    Basic Google Provider Authentication

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+  return (
+    <div style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh" 
+    }}>
+      <div style={{ textAlign: "center" }}>
+        <h1>Login</h1>
+
+        <button
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          style={{
+            padding: "10px 20px",
+            fontSize: "16px",
+            cursor: "pointer",
+            marginTop: "20px",
+          }}
+        >
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+}
